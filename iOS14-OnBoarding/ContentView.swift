bc7d367a8434fba25be51a40967ca6d263722f83
Jainam Shah <jainams@bu.edu>
2020-11-07 15:19:47 -0800

    Initial Commit

---
+++ iOS14-OnBoarding/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS14-OnBoarding
+//
+//  Created by Jainam Shah  on 11/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS14-OnBoarding/iOS14_OnBoardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS14_OnBoardingApp.swift
+//  iOS14-OnBoarding
+//
+//  Created by Jainam Shah  on 11/7/20.
+//
+
+import SwiftUI
+
+@main
+struct iOS14_OnBoardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
