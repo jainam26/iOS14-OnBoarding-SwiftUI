65d1320f82f823416a59c38499659b8500796aed
Jainam <jainam_s@icloud.comh>
2021-04-22 18:15:17 -0700

    uploaded Screens

--- iOS14-OnBoarding/ContentView.swift
+++ iOS14-OnBoarding/ContentView.swift
@@ -7,10 +7,35 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
+    
+    //@State private var textDictionary : [String:String] = ["Hello":"Get Started","Hola":"Empezar"]
+    //@State private var hellos = ["Hello","Hola"]
+    //@State private var getStartedes = ["Get Started","Empezar"]
+    //@State private var fadeInOut = false
+    //@State private var hello  = "Hello"
+    //@State private var getStarted = "Get Started"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView{
+            VStack{
+                Spacer()
+                Text("Hello")
+                    .font(.system(size: 46, weight: .semibold))
+                    .padding(.bottom)
+                Spacer()
+                
+                    NavigationLink(
+                        destination: LanguageView(),
+                        label: {
+                            Text("Get Started")
+                                .font(.headline)
+                })
+            }
+        }
     }
 }
 
---
+++ iOS14-OnBoarding/Screens/WiFiView.swift
@@ -0,0 +1,20 @@
+//
+//  WiFiView.swift
+//  iOS14-OnBoarding
+//
+//  Created by Jainam Shah  on 11/8/20.
+//
+
+import SwiftUI
+
+struct WiFiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WiFiView_Previews: PreviewProvider {
+    static var previews: some View {
+        WiFiView()
+    }
+}
